0582a1761324f9717e0f500d695843c5037fd53f
Marko Maturino <76288157+hybridmarko720@users.noreply.github.com>
2022-09-22 01:45:37 -0500

    First Commit.
    I was working on this for about an hour. I figured I should probably work push it to Github.

---
+++ js/index.js
@@ -0,0 +1,24 @@
+//Example fetch using pokemonapi.co
+function capitalizeFirstLetter(str) {
+    return str.charAt(0).toUpperCase() + str.slice(1);
+}
+
+document.querySelector('button').addEventListener('click', getFetch)
+
+function getFetch(){
+  const choice = document.querySelector('input').value.toLowerCase()
+  const url = `https://pokeapi.co/api/v2/pokemon/${choice}`
+  alert("HELLO WORLD")
+  fetch(url)
+      .then(res => res.json()) // parse response as JSON
+      .then(data => {
+        console.log(data)
+  
+        document.querySelector(".pokemon").innerText =  capitalizeFirstLetter(data.name)
+        document.querySelector('.pokemonPicture').src  = data.sprites.other["official-artwork"]["front_default"]
+        
+      })
+      .catch(err => {
+          console.log(`error ${err}`)
+      });
+}
